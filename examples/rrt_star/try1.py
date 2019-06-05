5b9c150980c77d56aca7c66b2740b88c430969bd
GPrathap <ggeesara@gmil.com>
2019-06-05 23:30:44 +0300

    reading random sample file

--- examples/rrt_star/try1.py
+++ examples/rrt_star/try1.py
@@ -89,38 +89,38 @@ from scipy import interpolate
 from mpl_toolkits.mplot3d import Axes3D
 
 
-# 3D example
-total_rad = 10
-z_factor = 3
-noise = 0.1
-
-num_true_pts = 200
-s_true = np.linspace(0, total_rad, num_true_pts)
-x_true = np.cos(s_true)
-y_true = np.sin(s_true)
-z_true = s_true/z_factor
-
-num_sample_pts = 80
-s_sample = np.linspace(0, total_rad, num_sample_pts)
-x_sample = np.cos(s_sample) + noise * np.random.randn(num_sample_pts)
-y_sample = np.sin(s_sample) + noise * np.random.randn(num_sample_pts)
-z_sample = s_sample/z_factor + noise * np.random.randn(num_sample_pts)
-
-from scipy.interpolate import splprep, splev
-
-tck, u = splprep([x_sample,y_sample,z_sample], s=2)
-x_knots, y_knots, z_knots = interpolate.splev(tck[0], tck)
-u_fine = np.linspace(0,1,num_true_pts)
-x_fine, y_fine, z_fine = interpolate.splev(u_fine, tck)
-
-fig2 = plt.figure(2)
-ax3d = fig2.add_subplot(111, projection='3d')
-ax3d.plot(x_true, y_true, z_true, 'b')
-ax3d.plot(x_sample, y_sample, z_sample, 'r*')
-ax3d.plot(x_knots, y_knots, z_knots, 'go')
-ax3d.plot(x_fine, y_fine, z_fine, 'g')
-fig2.show()
-plt.show()
+# # 3D example
+# total_rad = 10
+# z_factor = 3
+# noise = 0.1
+#
+# num_true_pts = 200
+# s_true = np.linspace(0, total_rad, num_true_pts)
+# x_true = np.cos(s_true)
+# y_true = np.sin(s_true)
+# z_true = s_true/z_factor
+#
+# num_sample_pts = 80
+# s_sample = np.linspace(0, total_rad, num_sample_pts)
+# x_sample = np.cos(s_sample) + noise * np.random.randn(num_sample_pts)
+# y_sample = np.sin(s_sample) + noise * np.random.randn(num_sample_pts)
+# z_sample = s_sample/z_factor + noise * np.random.randn(num_sample_pts)
+#
+# from scipy.interpolate import splprep, splev
+#
+# tck, u = splprep([x_sample,y_sample,z_sample], s=2)
+# x_knots, y_knots, z_knots = interpolate.splev(tck[0], tck)
+# u_fine = np.linspace(0,1,num_true_pts)
+# x_fine, y_fine, z_fine = interpolate.splev(u_fine, tck)
+#
+# fig2 = plt.figure(2)
+# ax3d = fig2.add_subplot(111, projection='3d')
+# ax3d.plot(x_true, y_true, z_true, 'b')
+# ax3d.plot(x_sample, y_sample, z_sample, 'r*')
+# ax3d.plot(x_knots, y_knots, z_knots, 'go')
+# ax3d.plot(x_fine, y_fine, z_fine, 'g')
+# fig2.show()
+# plt.show()
 def draw_from_ellipsoid(covmat, cent, npts):
     # random uniform points within ellipsoid as per: http://www.astro.gla.ac.uk/~matthew/blog/?p=368
     ndims = covmat.shape[0]
@@ -134,35 +134,43 @@ def draw_from_ellipsoid(covmat, cent, npts):
 
     # generate radii of hyperspheres
     rs = np.random.uniform(0, 1, npts)
-
+    # rs = np.arange(npts)
     # generate points
-    pt = np.random.normal(0, 1, [npts, ndims]);
+    pt = np.random.normal(0, 1, [npts, ndims])
+    # pt = np.arange(npts*ndims).reshape(npts, ndims)
 
     # get scalings for each point onto the surface of a unit hypersphere
     fac = np.sum(pt ** 2, axis=1)
 
     # calculate scaling for each point to be within the unit hypersphere
     # with radii rs
+
     fac = (rs ** (1.0 / ndims)) / np.sqrt(fac)
 
-    pnts = np.zeros((npts, ndims));
+    pnts = np.zeros((npts, ndims))
 
     # scale points to the ellipsoid using the eigenvalues and rotate with
     # the eigenvectors and add centroid
     d = np.sqrt(np.diag(e))
     d.shape = (ndims, 1)
 
+    print(v)
     for i in range(0, npts):
         # scale points to a uniform distribution within unit hypersphere
         pnts[i, :] = fac[i] * pt[i, :]
+        print(np.multiply(pnts[i, :], np.transpose(d)))
         pnts[i, :] = np.dot(np.multiply(pnts[i, :], np.transpose(d)), np.transpose(v)) + cent
+
 
     return pnts
 
 
 
 covmat = np.diag((7, 4, 4))
-pnts = draw_from_ellipsoid(covmat, 0, 100000)
+# pnts = draw_from_ellipsoid(covmat, 0, 10)
+
+samples = np.load('/dataset/1random_samples.npy')
+pnts = samples
 plt.scatter(pnts[:,0], pnts[:,1], pnts[:,2])
 plt.show()
 
